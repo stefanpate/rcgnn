002482754d4c4096a1005d599cd8a0be65ab03dc
stefanpate <stefanpate94@gmail.com>
2025-01-08 16:30:55 -0600

    Clean up old code

--- src/filepaths.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from omegaconf import OmegaConf
-
-project_dir = Path(__file__).parents[1]
-filepaths = OmegaConf.load(project_dir / "configs" / "filepaths" / "base.yaml")
-
-for k in filepaths.keys():
-    filepaths[k] = Path(filepaths[k])
