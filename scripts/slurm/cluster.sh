47f3382e2a8fdf6760f0d5debdda95fefcfe3e9a
stefanpate <stefanpate94@gmail.com>
2025-10-06 15:56:46 -0500

    Clustered esm. Staged drfp and esm split

--- scripts/slurm/cluster.sh
+++ scripts/slurm/cluster.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 1
 #SBATCH --mem=64GB
 #SBATCH -t 3:00:00
-#SBATCH --job-name=sim_mat
+#SBATCH --job-name=clstr
 #SBATCH --output=/home/spn1560/hiec/logs/out/%x_%A.out
 #SBATCH --error=/home/spn1560/hiec/logs/error/%x_%A.err
 #SBATCH --mail-type=END
@@ -14,11 +14,14 @@
 
 # Args
 scripts_dir=/home/spn1560/hiec/scripts
+similarity_score=esm
+dataset=sprhea
+toc=v3_folded_pt_ns
 
 # Commands
 ulimit -c 0
 module purge
 module load gcc/9.2.0
 module load python-miniconda3/4.12.0
-source activate /home/spn1560/.conda/envs/hiec2
-python $scripts_dir/similarity_matrix.py esm sprhea v3_folded_pt_ns
+source activate /home/spn1560/.conda/envs/hiec
+python $scripts_dir/cluster.py similarity_score=$similarity_score dataset=$dataset toc=$toc
--- src/similarity.py
+++ src/similarity.py
@@ -732,7 +732,7 @@ def _wrap_rxn_tani(args):
     return reaction_tanimoto_similarity(*args)
 
 def load_similarity_matrix(sim_path: Path, dataset: str, toc: str, sim_metric: str, dtype: np.dtype = np.float32):
-    if sim_metric in ['rcmcs', 'mcs', 'tanimoto', 'agg_mfp_cosine', 'drfp', 'unaligned_tanimoto']:
+    if sim_metric in ['rcmcs', 'mcs', 'tanimoto', 'agg_mfp_cosine', 'drfp', 'unaligned_tanimoto', 'esm']:
         S = np.load(
             sim_path / f"{dataset}_{toc}_{sim_metric}.npy"
         ).astype(np.float32)
