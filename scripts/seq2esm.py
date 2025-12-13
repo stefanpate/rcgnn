ac68884621968e0dec2b2de47428707ea9d38883
stefanpate <stefanpate94@gmail.com>
2025-12-13 12:01:24 -0600

    Formatted time split for clipzyme, made min rule compare of 24 and 25 sprhea

--- scripts/seq2esm.py
+++ scripts/seq2esm.py
@@ -4,10 +4,11 @@ from hydra import initialize, compose
 with initialize(config_path="../configs/filepaths", version_base=None):
     cfg = compose(config_name="base")
 
-outdir = f"{cfg.data}/time_split/esm"
+outdir = f"{cfg.data}/time_split/esm2_repeat"
 fasta_path = f"{cfg.data}/time_split/250915_time_split.fasta"
-model_loc = f"{cfg.pretrained_models}/esm1b_t33_650M_UR50S.pt"
-_include = "mean"
+# model_loc = f"{cfg.pretrained_models}/esm2_t33_650M_UR50D.pt"
+model_loc = 'esm2_t33_650M_UR50D'
+_include = "per_tok"
 
 ensure_dirs(outdir)
 retrive_esm1b_embedding(fasta_path, outdir, model_loc, _include)
